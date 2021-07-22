3e6128a440a86a582635aea1afe9da221c08a60c
Tiffany Wise <TiffanyJWise84@gmail.com>
2021-07-21 20:37:23 -0400

    Refactored code for checkbox group and selectall checkbox

--- src/index.js
+++ src/index.js
@@ -1,42 +1,43 @@
-var selectAll = document.querySelector('input[id="select-all"]');
-var checkboxGroup = document.querySelectorAll('input[name="colors"]');
+var select_all_checkbox = document.querySelector('input[id="select-all"]');
+var colors_checkbox_group = document.querySelectorAll('input[name="colors"]');
 
-// assign event listeners to individual checkboxes
-for (let i = 0; i < checkboxGroup.length; i++) {
-  checkboxGroup[i].addEventListener('click', changeSelectAll);
-}
 
-// assign event listener for select all checkbox
-selectAll.addEventListener('click', function() {
-  if (selectAll.checked) {
-    changeCheckboxGroup(true);
-  } else {
-    changeCheckboxGroup(false);
-  }
-  // change checkbox group to match select all checkbox
-  function changeCheckboxGroup(status) {
-    for (let i = 0; i < checkboxGroup.length; i++) {
-      checkboxGroup[i].checked = status;
-    }
-  }
-});
-
-function changeSelectAll() {
-  if (checkboxGroupStatus(checkboxGroup)) {
-    selectAll.checked = true;
-  } else {
-    selectAll.checked = false;
-  }
-}
-
-function checkboxGroupStatus(checkBoxGroup) {
-  let boxesChecked = 0;
-  for (let i = 0; i < checkBoxGroup.length; i++) {
-    if (checkBoxGroup[i].checked) {
-      boxesChecked++;
+if (select_all_checkbox && colors_checkbox_group) {
+  // event listener for select all checkbox
+  select_all_checkbox.addEventListener("click", function() {
+    if (select_all_checkbox.checked) {
+      changeGroup(colors_checkbox_group, true);
     } else {
+      changeGroup(colors_checkbox_group, false);
+    }
+  });
+  // event listeners on each checkbox
+  for (let i = 0; i < colors_checkbox_group.length; i++) {
+    colors_checkbox_group[i].addEventListener("click", function() {
+      if (allBoxesAreChecked(colors_checkbox_group)) {
+        select_all_checkbox.checked = true;
+      } else {
+        select_all_checkbox.checked = false;
+      }
+    })
+  }
+}
+
+// function to check status of checkbox in group
+function allBoxesAreChecked(group) {
+  let boxesChecked = 0;
+  for (let i = 0; i < group.length; i++) {
+    if (group[i].checked === true) {
+      boxesChecked++;
+    } else if (boxesChecked < i + 1) {
       return false;
     }
   }
-  return boxesChecked == checkBoxGroup.length;
+  return boxesChecked === group.length;
+}
+
+function changeGroup(group, newStatus) {
+  for (let i = 0; i < group.length; i++) {
+    group[i].checked = newStatus;
+  }
 }
