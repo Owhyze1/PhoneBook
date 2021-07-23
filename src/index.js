d9a6fe1811c8c169a4d4fcae83ec553419262257
Tiffany Wise <TiffanyJWise84@gmail.com>
2021-07-23 09:12:57 -0400

    Refactored select all button code

--- src/index.js
+++ src/index.js
@@ -1,43 +1,35 @@
-var select_all_checkbox = document.querySelector('input[id="select-all"]');
-var colors_checkbox_group = document.querySelectorAll('input[name="colors"]');
+var selectAllCheckbox = document.getElementById('select-all');
+var checkboxGroup = document.querySelectorAll('input[name="colors"]');
 
-
-if (select_all_checkbox && colors_checkbox_group) {
-  // event listener for select all checkbox
-  select_all_checkbox.addEventListener("click", function() {
-    if (select_all_checkbox.checked) {
-      changeGroup(colors_checkbox_group, true);
-    } else {
-      changeGroup(colors_checkbox_group, false);
+if (selectAllCheckbox !== null) {
+  if (checkboxGroup !== null) {
+    selectAllCheckbox.addEventListener('click', function() {
+      changeGroupStatus(checkboxGroup, selectAllCheckbox.checked);
+    });
+    for (let i = 0; i < checkboxGroup.length; i++) {
+      checkboxGroup[i].addEventListener('click', function() {
+        selectAllCheckbox.checked = checkboxGroupStatus(checkboxGroup);
+      });
     }
-  });
-  // event listeners on each checkbox
-  for (let i = 0; i < colors_checkbox_group.length; i++) {
-    colors_checkbox_group[i].addEventListener("click", function() {
-      if (allBoxesAreChecked(colors_checkbox_group)) {
-        select_all_checkbox.checked = true;
-      } else {
-        select_all_checkbox.checked = false;
-      }
-    })
   }
 }
 
-// function to check status of checkbox in group
-function allBoxesAreChecked(group) {
-  let boxesChecked = 0;
-  for (let i = 0; i < group.length; i++) {
-    if (group[i].checked === true) {
-      boxesChecked++;
-    } else if (boxesChecked < i + 1) {
-      return false;
+function changeGroupStatus(group, status) {
+  if (group !== null) {
+    for (let i = 0; i < group.length; i++) {
+      group[i].checked = status;
     }
   }
-  return boxesChecked === group.length;
 }
 
-function changeGroup(group, newStatus) {
-  for (let i = 0; i < group.length; i++) {
-    group[i].checked = newStatus;
+function checkboxGroupStatus(group) {
+  if (group === null) return group;
+
+  let startingStatus = group[0].checked;
+  for (let i = 1; i < group.length; i++) {
+    if (group[i].checked !== startingStatus) {
+      return null;
+    }
   }
+  return startingStatus;
 }
